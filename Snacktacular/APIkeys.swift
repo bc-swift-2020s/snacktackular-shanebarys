07f016d5922e4018ff44c0a36328d2c1120534b1
Shane Barys <shanejbarys@gmail.com>
2020-04-13 00:41:56 -0500

    9.4

---
+++ Snacktacular/APIkeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIkeys.swift
+//  Snacktacular
+//
+//  Created by Shane Barys on 4/13/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBlCiChqaLjDeqmrmcU_s0hTV0vw2UfFqY"
+}
+
